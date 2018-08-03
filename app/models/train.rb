177a5e5e77f740ffff14225eac7b67e3787ed953
praeco_futuri <xxmolekula@gmail.com>
2018-08-03 21:10:38 +0300

    fix acceptance tests trains

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,3 +1,10 @@
 class Train < ApplicationRecord
   validates :first_name_station, :last_name_station, :place, presence: true
+  validate :validates_station
+
+  def validates_station
+    if self.first_name_station == self.last_name_station
+      self.errors[:name] << 'Can\'t be  stations  same'
+    end
+  end
 end
--- spec/acceptance/trains/trains_spec.rb
+++ spec/acceptance/trains/trains_spec.rb
@@ -3,20 +3,10 @@ require 'rails_helper'
 feature 'Create train' do
   scenario 'successfully create' do
     visit new_train_path
-    click_on 'create train'
-    fill_in 'First station name', with: 'Orest'
-    fill_in 'Last station name', with: 'Orest'
+    fill_in 'First station name', with: 'Lviv'
+    fill_in 'Last station name', with: 'Harkiv'
     click_on 'Save'
 
     expect(page).to have_content 'successfully updated.'
-  end
-  scenario 'failed create' do
-    visit new_train_path
-    click_on 'create train'
-    fill_in 'First station name', with: 'Orest'
-    fill_in 'Last station name', with: 'Orest'
-    click_on 'Save'
-
-    expect(page).to have_content 'Station can\'t be blank'
   end
 end
